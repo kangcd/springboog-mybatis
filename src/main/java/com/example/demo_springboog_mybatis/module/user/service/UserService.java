1cd3546e556b674225e73c1a9547a3e646aac12f
康成东 <kangcd@dtinsure.com>
2018-07-10 14:35:00 +0800

    初始化项目

---
+++ src/main/java/com/example/demo_springboog_mybatis/module/user/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.demo_springboog_mybatis.module.user.service;
+
+
+import com.example.demo_springboog_mybatis.module.user.entity.User;
+
+public interface UserService {
+    User getUser(String id);
+}
